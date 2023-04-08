275bbe2a00ddc53539e49fc455d38a17a7b5b82c
Val K <valq7711@gmail.com>
2023-04-09 02:39:51 +0300

    Allow to pass data via `XTemplate`

--- upytl.js
+++ upytl.js
@@ -1,7 +1,8 @@
 upytl = {}
 
-upytl.mount_component = function(id){
+upytl.mount_component = function(id, data){
+    data = data || {}
     var id = `#${id}`
-    var c = Vue.extend({template: id})
+    var c = Vue.extend({template: id, data: ()=>data})
     new c().$mount(id)
 }
--- upytl/xtemplate.py
+++ upytl/xtemplate.py
@@ -1,22 +1,28 @@
 import uuid
-from typing import TypeVar, Type
+from typing import TypeVar, Type, Union
+import json
 
 from . import html as h
 from .core import Component, Slot, Tag
 
 
 class XTemplate(Component):
-    props = []
+
+    def __init__(self, data: Union[dict, str] = b'{}', id: str = '', **kw):
+        super().__init__(data=data, id=id, **kw)
 
     def get_context(self, props_rendered: dict) -> dict:
-        if 'id' not in props_rendered:
+
+        if not props_rendered['id']:
             props_rendered['id'] = f'_{uuid.uuid4()}'
+        data = props_rendered['data']
+        if isinstance(data, dict):
+            props_rendered['data'] = json.dumps(data, ensure_ascii=False, default=str, separators=(',', ':'))
         return props_rendered
 
     template = {
         h.Script(type='text/x-template', id='{id}'): {
             Slot(): ''
         },
-        h.Script(): 'upytl.mount_component("[[id]]")'
+        h.Script(): 'upytl.mount_component("[[id]]", [[data]])'
     }
-
